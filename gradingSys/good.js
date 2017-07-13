4a6efd002b01eadbddc0065f5fef36e445f56f2a
Andree Peyrot <andreepeyrot@att.net>
2017-07-13 13:13:10 -0400

    first commit

---
+++ gradingSys/good.js
@@ -0,0 +1,109 @@
+
+// let myGrade = "A";
+// if (90=<100)
+// let myGrade = "B";
+// if else  (80=<89)
+// let myGrade = "C";
+// if else (70=>79)
+// let myGrade = "D";
+// if else(60=>69)
+// let myGrade = "F";
+// if else(59>0)
+// console.log(myGrade);
+// function myGrade(){}
+
+// average(0<100). average
+// average([1]) #1.0
+// average([0,2]) #1.0
+// average([1,2]) #1.5
+// function changeText(form) {
+//   id.innerHTML = "Ooops !, Please enter a number between 0 and 100.";
+
+//if (input type="100<") {
+//  alert("Please enter a number between 0 and 100")
+// if onclick=" ":
+
+
+var letterGrade = ("0>=100");
+
+function mynumberGrade(){ if (letterGrade = "90<=100") {
+  var x = document.getElementsByName("mynumberGrade");
+  document.getElementById("onclick").innerHTML = "mynumberGrade" + letterGrade;
+
+    console.log("You have an "A"");
+
+} else { if (letterGrade = "80<=89")
+    console.log("You have a "B"");
+
+} else { if (letterGrade = "70<=79")
+    console.log("You have a "C"");
+
+} else { if (letterGrade = "60<=69")
+    console.log("You have a "D"");
+
+} else { if (letterGrade = "0<=59")
+    console.log("You need a Tutor");
+
+}
+
+
+// function myaverageGrade = (){
+//   let myaverageGrade = ["A" + "B" + "C" + "D" + "F"/divided by how many entires];
+//   console.log(myaverageGrade);
+// }
+// myaverageGrade();
+// mynumberGrade ();
+// addEventListener("click",
+//   function() {
+//     alert("click");
+//
+//
+//     // function myFunction(){
+//     //     document.getElementById("pf").innerHTML="A";
+//     // }
+//
+// var n = prompt("Enter")
+//
+//
+// let id1 = mynumberGrade [0]; //between 0>100
+// let id2 = mynumberGrade [1]; //between 0>100
+// let id3 = mynumberGrade [2]; //between 0>100
+// let id4 = mynumberGrade [3]; //between 0>100
+// let id5 = mynumberGrade [4]; //between 0>100
+//
+//   }
+// mynumberGrade.length;//5
+// let myletterGrade = ["A", "B", "C","D","F"];
+// let id1 = myletterGrade [0];//a
+// let id2 = myletterGrade [1];//b
+// let id3 = myletterGrade [2];//c
+// let id4 = myletterGrade [3];//d
+// let id5 = myletterGrade [4];//f
+//
+// console.log('myletterGrade'+ "." + "text");
+// addEventListener("click",
+//   function() {
+//     alert("click");
+//   }
+// function myFunction(){
+//   var avergeNumbers = 0;
+//   var averageL= 0;
+//   var averageNumbergrade = 0;
+//   var averageLettergrade = 0;
+//   var wildCh = 0;
+//
+// for(var i = 0; i <= x.length; i++){
+//   if(x[i] == " "){
+//   spaceCounter++;
+//   wordCounter++;
+//
+//   if( x[i] == "." || x[i] == "?" || x[i] == "!"){
+//     sentenceCounter++;
+//   }
+// }
+// avergeNumbergrade = numberAdd/numberentry;
+// document.getElementById('id1').innerHTML = "You have an A" + avergeNumbergrade;
+// document.getElementById('id2').innerHTML = "You have a B" + avergeNumbergrade;
+// document.getElementById('id3').innerHTML = "You have a C" + averageNumbergrade;
+// document.getElementById('id4').innerHTML = "You have a D" + averageNumbergrade;
+// document.getElementById('id5').innerHTML = "You have an F" + averageNumbergrade;
