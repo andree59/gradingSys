7de0aebbe3e0f3292f7bb2b9c930b2d131fa180d
Andree Peyrot <andreepeyrot@att.net>
2017-07-19 16:27:29 -0400

    2nd  commit

--- gradingSys/good.js
+++ gradingSys/good.js
@@ -1,4 +1,44 @@
 
+
+
+var myletterGrade = 0 <=100;
+var myButton = document.getElementById("myButton");
+var input = document.getElementById('input');
+myButton.addEventListener("click", myletterGrade(){
+
+
+if (myletterGrade === "90<=100") {
+  alert ("You have an A")
+
+}
+else if (myletterGrade === "80<= 89") {
+
+  alert ("You have a B")
+
+}
+  else if (myletterGrade === "70<=79") {
+
+  alert ("You have a C")
+}
+    else if (myletterGrade === "60<=69") {
+
+  alert ("You have a D")
+}
+      else if (myletterGrade === "0<=59") {
+
+  alert ("You need a Tutor")
+  }
+}
+
+
+
+
+
+
+
+
+
+
 // let myGrade = "A";
 // if (90=<100)
 // let myGrade = "B";
@@ -24,28 +64,78 @@
 // if onclick=" ":
 
 
-var letterGrade = ("0>=100");
 
-function mynumberGrade(){ if (letterGrade = "90<=100") {
-  var x = document.getElementsByName("mynumberGrade");
-  document.getElementById("onclick").innerHTML = "mynumberGrade" + letterGrade;
 
-    console.log("You have an "A"");
-
-} else { if (letterGrade = "80<=89")
-    console.log("You have a "B"");
-
-} else { if (letterGrade = "70<=79")
-    console.log("You have a "C"");
-
-} else { if (letterGrade = "60<=69")
-    console.log("You have a "D"");
-
-} else { if (letterGrade = "0<=59")
-    console.log("You need a Tutor");
-
+// // function mynumberGrade() {
+// //   document.getElementbyName("grade").innerHTML = " ";
+// var content = document.getElementById("grade");
+// var button = document.getElementById("myletterGrade");
+//
+// function addListeners(){
+//   if(window.addEventListener) {
+//       document.getElementById ('myletterGrade').addEventListener(
+//         "click",letterGrade, false);
+//   } else if (window.attachEvent){}
+//
+// function letterGrade(){
+//
+//   alert (this.id+" you have an a");
+// }
+// window.onload = addListeners;
+//
+// var = 0 <= 100;
+// function letterGrade(){
+//     if("90 <= 100"){
+//       alert("You have an "A"");
+//     } else  {
+//       myletterGrade == "80 <= 89"{
+//          alert ("You have a "B"");
+//
+//        } else  {
+//          myletterGrade == "70 <= 79"{
+//             alert ("You have a "C"");
+//
+//           } else  {
+//             myletterGrade == "60 <= 69"{
+//                alert ("You have a "D"");
+//
+//              } else {
+//                myletterGrade == "0 <= 59"{
+//                   alert ("You need a Tutor");
+//                 }
 }
+//   var letterGrade = ("90 <= 100");
+//   var letterGrade1 = ("80 <= 89");
+//   var letterGrade2 = ("70 <= 79");
+//   var letterGrade3 = ("60 <= 69");
+//   var letterGrade4 = ("50 <= 59");
+//   var letterGrade5 =  ("0 <= 49"):
+//   document.getElementById("grade").innerHTML= letterGrade, letterGrade1;letterGrade2,
+//   letterGrade3, letterGrade4, letterGrade5;
+// }
 
+//   if (letterGrade =="90 <= 100") {
+//   // var x = document.getElementsByName("mynumberGrade");
+//   // document.getElementById("onclick").innerHTML = "mynumberGrade" + letterGrade;
+//
+//     alert("You have an "A"");
+//
+//   } else if {(letterGrade == "80 <= 89")
+//     alert ("You have a "B"");
+//
+//     } else if (letterGrade == " 70 <= 79")
+//       alert ("You have a "C"");
+//
+//       } else if (letterGrade == "60 <= 69")
+//         alert ("You have a "D"");
+//
+//         } else if (letterGrade == "0 <= 59")
+//           alert ("You need a Tutor");
+//
+//  }
+//
+// }
+//
 
 // function myaverageGrade = (){
 //   let myaverageGrade = ["A" + "B" + "C" + "D" + "F"/divided by how many entires];
