7f46add79a8319d9ee596cf14eb1fb359cb3f79b
Andree Peyrot <andreepeyrot@att.net>
2017-07-21 16:50:41 -0400

    third commit

--- gradingSys/good.js
+++ gradingSys/good.js
@@ -1,30 +1,37 @@
 
 
 
-var myletterGrade = 0 <=100;
+var grade = (0,1,2,3,4,5,6,7,8,9,10,
+  11,12,13,14,15,16,17,18,119,20,
+  21,22,23,24,25,26,27,28,29,30,
+  31,32,33,34,35,36,37,38,39,40,
+  41,42,43,44,45,46,47,48,49,50,
+  51,52,53,54,55,56,57,58,59,60,
+  62,62,63,64,65,66,67,68,69,70,
+  71,72,73,74,75,76,77,78,79,80,
+  81,82,83,84,85,86,87,88,89,90
+  91,92,93,94,95,96,97,98,99,100);
 var myButton = document.getElementById("myButton");
-var input = document.getElementById('input');
-myButton.addEventListener("click", myletterGrade(){
+myButton.addEventListener("click", grade(){
 
 
-if (myletterGrade === "90<=100") {
+if (grade === "90<=100") {
   alert ("You have an A")
 
 }
-else if (myletterGrade === "80<= 89") {
-
+else if (grade==="80<=89")
   alert ("You have a B")
 
 }
-  else if (myletterGrade === "70<=79") {
+  else if (grade === "70<=79") {
 
   alert ("You have a C")
 }
-    else if (myletterGrade === "60<=69") {
+    else if (grade === "60<=69") {
 
   alert ("You have a D")
 }
-      else if (myletterGrade === "0<=59") {
+      else if (grade === "0<=59") {
 
   alert ("You need a Tutor")
   }
